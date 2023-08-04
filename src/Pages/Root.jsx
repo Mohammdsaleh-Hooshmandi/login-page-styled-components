5819b555de642d62f7acccb8cf71dedef3f5f35a
Mohammdsaleh Hooshmandi <Mohammdsale.Hooshmandi1382@gmail.com>
2023-08-04 11:58:11 +0430

    add style to Root

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -1,14 +1,27 @@
 import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 
 import Header from "../Components/Header";
+
+const Layout = styled.main`
+    max-width: 70%;
+    margin: 0 auto;
+    padding: 5rem 0;
+
+    @media (max-width: 850px) {
+        max-width: 90%;
+    }
+`;
 
 function Root() {
 
     return (
         <Fragment>
             <Header />
-            <Outlet />
+            <Layout>
+                <Outlet />
+            </Layout>
         </Fragment>
     );
 }
